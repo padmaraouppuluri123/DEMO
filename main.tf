25ac3a7caf08e39ea690678ea74e8c53c5ce9388
padmarao <uppuluri.veera@gmail.com>
2025-02-07 05:55:09 +0000

    updated mainfile

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-# Specify the Google Cloud provider
-provider "google" {
-  credentials = file("C:/Users/hr316/Downloads/helpful-silo-449805-t4-a518f0f8157c.json")  # Corrected file path
-  project     = "helpful-silo-449805-t4"  # Replace with your actual Google Cloud project ID
-  region      = "us-central1"         # Define the region, e.g., us-central1
-}
-
-# Example resource: Google Compute Engine VM instance
-resource "google_compute_instance" "example-instance" {
-  name         = "example-instance"
-  machine_type = "f1-micro"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-9-stretch-v20210721"  # Replace with your preferred image
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}  # Automatically assigns an external IP
-  }
-}
 
 
 
