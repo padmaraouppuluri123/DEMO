1b55247c86cbe291cfe6d9de827d6266404a087d
padmaraouppuluri123 <50736612+padmaraouppuluri123@users.noreply.github.com>
2025-02-07 14:45:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  project     = "helpful-silo-449805"
+  region      = "us-central1"
+}
 
-
-
+resource "google_storage_bucket" "my_bucket" {
+  name          = "padmarao-demo-bucket"
+  location      = "US"
+}
