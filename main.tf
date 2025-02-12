3bf2cff93706d69801869dde451c46abbc1d215c
padmaraouppuluri123 <50736612+padmaraouppuluri123@users.noreply.github.com>
2025-02-12 18:45:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
 provider "google" {
-  project     = "helpful-silo-449805-t4"
-  region      = "us-central1"
+  project = "helpful-silo-449805-t4"
+  region  = "us-central1"
 }
 
+# Generate a random suffix for unique bucket names
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+  numeric = true
+}
+
+# Create a new GCS bucket with a unique name every time
 resource "google_storage_bucket" "my_bucket" {
-  name          = "bucket8889-padmarao"
+  name          = "bucket-${random_string.suffix.result}-padmarao"
   location      = "US"
+  storage_class = "STANDARD"
+}
+
+# Output the bucket name for reference
+output "bucket_name" {
+  value = google_storage_bucket.my_bucket.name
 }
